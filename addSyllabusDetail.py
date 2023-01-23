c68bd7cdd0bc19c6323476c5357209b7aa3289bf
Stead08 <inomatama08@gmail.com>
2023-01-23 19:34:25 +0900

    update 1/23/2023 fix addSyllabusDetail.py

--- addSyllabusDetail.py
+++ addSyllabusDetail.py
@@ -1,3 +1,4 @@
+# パッケージのインポート
 from bs4 import BeautifulSoup
 import pprint
 import psycopg2
@@ -5,16 +6,16 @@ from psycopg2 import Error
 import settings
 import glob
 import datetime
-
+# htmlを保存しているディレクトリでパスのリストを生成
 path_list=glob.glob("archived_scraped_data/syllabus_econ_2022/*")
 # pprint.pprint(path_list)
 
 class SyllabusDetail:
     def __init__(self, classYear):
         self.classYear = classYear
-
+# pathリスト分の空リストを生成し、そこに要素リストを代入する。
 syllabus_details_list = [None] * (len(path_list))
-# シラバスリストのパス
+# シラバスリストの各パスについて
 for i, path in enumerate(path_list):
     with open(path, encoding="utf-8") as f:
         html = f.read()
@@ -22,7 +23,7 @@ for i, path in enumerate(path_list):
     soup = BeautifulSoup(html, "lxml")
     tables = soup.find("table", class_="c-table-line")
     #<table>で取れる情報
-    t_list = [i.text.replace("\n", "").replace("\u3000", "") for i in tables.find_all("td")]
+    t_list = [i.text.replace("\n", "") for i in tables.find_all("td")]
 
     dayOfWeek1 = None
     period1 = None
@@ -34,7 +35,9 @@ for i, path in enumerate(path_list):
     elif len(t_list[9]) == 2:
         dayOfWeek1 = str(t_list[9])[0]
         period1 = int(str(t_list[9])[1])
-    if len(t_list[9]) == 5:
+    elif len(t_list[9]) == 5:
+        dayOfWeek1 = str(t_list[9])[0]
+        period1 = int(str(t_list[9])[1])
         dayOfWeek2 = str(t_list[9])[3]
         period2 = int(str(t_list[9])[4])
     lesson = t_list[1]
@@ -42,6 +45,8 @@ for i, path in enumerate(path_list):
     instructor = t_list[3]
     grade = t_list[5]
     semester = t_list[7]
+    if semester == '前期\u3000～\u3000後期（通年）':
+        semester = "前期"
     semesterAlt = t_list[8]
     lessonCategory = t_list[10]
     subjectCategory = t_list[11]
@@ -95,22 +100,40 @@ connection = psycopg2.connect(
 
 )
 
+def insertSyllabusDetails(syllabus_details_list: list):
+    '''
+    :param syllabus_details_list:
+    syllabus_details_listをポスグレにinsertして、インデックステーブにIDを登録
+    '''
+    syllabus_list_id_scraping = None
+    for syllabus_detail in syllabus_details_list:
+        print(syllabus_detail)
+        with connection:
+            try:
+                with connection.cursor() as cursor:
+                    sql1 = 'INSERT INTO "SyllabusDetails"("講義名", "クラス", "担当教員", "学年", "開講学期", "開講時期", "曜日1", "時限1", "曜日2", "時限2", "科目種別", "科目区分", "単位区分", "単位数", "準備事項", "備考", "特殊プログラム", "授業方法", "講義情報", registration_date) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) RETURNING syllabus_detail_id;'
+                    cursor.execute(sql1, syllabus_detail)
+                    syllabus_detail_id = cursor.fetchone()
+                    sql2 = 'SELECT "syllabus_list_id" FROM "SyllabusList" WHERE "講義名" = %s AND "担当教員" = %s AND "開講時期" = %s AND "曜日1" = %s'
+                    cursor.execute(sql2, [syllabus_detail[0] ,syllabus_detail[2], syllabus_detail[4], syllabus_detail[6]])
+                    syllabus_list_id_scraping = cursor.fetchone()
+                    if syllabus_list_id_scraping == None:
+                        print("None")
+                        continue
+                    syllabus_list_id_scraping = syllabus_list_id_scraping[0]
 
-with connection:
-    try:
-        with connection.cursor() as cursor:
-            #sql = 'INSERT INTO "SyllabusList" ("年度", "カテゴリ", "科目区分", "単位区分", "講義名", "担当教員", "学年", "開講時期", "曜日1", "時限1", "曜日2", "時限2", "科目コード", "ナンバリング", syllabus_detail_id) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);'
-            sql = 'INSERT INTO "SyllabusDetails"("講義名", "クラス", "担当教員", "学年", "開講学期", "開講時期", "曜日1", "時限1", "曜日2", "時限2", "科目種別", "科目区分", "単位区分", "単位数", "準備事項", "備考", "特殊プログラム", "授業方法", "講義情報", registration_date) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) RETURNING syllabus_detail_id;'
-            cursor.executemany(sql, syllabus_details_list)
-    except Error as error:
-        print(error)
+                    sql3 = 'UPDATE "SyllabusList" SET "syllabus_detail_id" = %s WHERE "syllabus_list_id" = %s'
+                    cursor.execute(sql3, [syllabus_detail_id, syllabus_list_id_scraping])
 
-    #コミットしてトランザクション実行
-    connection.commit()
+            except Error as error:
+                print(error)
 
-# 終了処理
-cursor.close()
+            #コミットしてトランザクション実行
+            connection.commit()
 
+        # 終了処理
+        cursor.close()
 
+insertSyllabusDetails(syllabus_details_list)
 
 
